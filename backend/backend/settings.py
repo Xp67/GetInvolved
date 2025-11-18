be5971d07c4bfffb9bef0909bdca88d409ec7876
Xp67 <marco.Def4lt@gmail.com>
2025-11-18 19:45:52 +0100

    reverse

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '..\env\Lib\site-packages\rest_framework\static'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
